b59ed2b85cd9489c292960d646b6f5415ae91c98
Kelvin Lee <lee850220@gmail.com>
2019-07-12 22:30:51 +0800

    spalsh_page

---
+++ app/src/main/java/com/kelvin/kanime/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.kelvin.kanime
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPALSH_TIME_OUT : Long = 3000 // 3 sec
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPALSH_TIME_OUT)
+    }
+}
